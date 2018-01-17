1ddebad07571cbbc44e1d3e13c130e8a914c5f9a
Peter <peterbupt@gmail.com>
2018-01-17 14:48:45 +0800

    AES ok

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/gob"
+	"port-forward/models"
+	_ "port-forward/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+
+	logs.SetLogger(logs.AdapterConsole, `{"level":7}`)
+	logs.SetLogger(logs.AdapterFile, `{"filename":"app.log","level":7,"maxlines":0,"maxsize":0,"daily":true,"maxdays":10}`)
+
+	//输出文件名和行号
+	logs.EnableFuncCallDepth(true)
+	logs.SetLogFuncCallDepth(3)
+	//为了让日志输出不影响性能，开启异步日志
+	logs.Async()
+
+	//开启seesion支持，默认使用的存储引擎为：memory
+	beego.BConfig.WebConfig.Session.SessionOn = true
+	beego.BConfig.WebConfig.Session.SessionName = "sessionID"
+	beego.BConfig.WebConfig.Session.SessionGCMaxLifetime = 3600
+	//beego.BConfig.WebConfig.Session.SessionProvider = "file"
+	//beego.BConfig.WebConfig.Session.SessionProviderConfig = "./session"
+	gob.Register(&models.LoginUser{})
+
+	logs.Debug("================================================================")
+	logs.Debug("               SDN网元数据保护系统 启动...")
+	logs.Debug("    _____        _           _____           _            _")
+	logs.Debug("   |  __ \\      | |         |  __ \\         | |          | |")
+	logs.Debug("   | |  | | __ _| |_ __ _   | |__) | __ ___ | |_ ___  ___| |_")
+	logs.Debug("   | |  | |/ _` | __/ _` |  |  ___/ '__/ _ \\| __/ _ \\/ __| __|")
+	logs.Debug("   | |__| | (_| | || (_| |  | |   | | | (_) | ||  __/ (__| |_")
+	logs.Debug("   |_____/ \\__,_|\\__\\__,_|  |_|   |_|  \\___/ \\__\\___|\\___|\\__|")
+	logs.Debug("")
+	logs.Debug("=================================================================")
+
+	//默认static目录是可以直接访问的，其它目录需要单独指定
+	beego.SetStaticPath("/theme", "theme")
+
+	//启动应用
+	beego.Run()
+
+}
---
+++ utils/CryptoUtil.go
@@ -0,0 +1,55 @@
+package utils
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"crypto/aes"
+	"os"
+	"crypto/cipher"
+)
+
+//生成32位md5字串
+func GetMd5(input string) string {
+	hash := md5.New()
+	hash.Write([]byte(input))
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+func GetSaltMD5(input, salt string) string {
+	hash := md5.New()
+	//salt = "salt123456" //盐值
+	io.WriteString(hash, input+salt)
+	result := fmt.Sprintf("%x", hash.Sum(nil))
+	return result
+}
+
+func GetCipherText(input []byte,key string)[]byte{
+	var commonIV = []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
+	// 创建加密算法aes
+	c, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		fmt.Printf("Error: NewCipher(%d bytes) = %s", len(key), err)
+		os.Exit(-1)
+	}
+	cfb := cipher.NewCFBEncrypter(c, commonIV)
+	ciphertext := make([]byte, len(input))
+	cfb.XORKeyStream(ciphertext, input)
+	return ciphertext
+}
+
+
+func GetPlainText(input []byte,key string)[]byte{
+	var commonIV = []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
+	// 创建加密算法aes
+	c, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		fmt.Printf("Error: NewCipher(%d bytes) = %s", len(key), err)
+		os.Exit(-1)
+	}
+	cfbdec := cipher.NewCFBDecrypter(c, commonIV)
+	plaintextCopy := make([]byte, len(input))
+	cfbdec.XORKeyStream(plaintextCopy, input)
+	return plaintextCopy
+}
